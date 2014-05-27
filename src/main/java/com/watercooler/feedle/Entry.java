6138885f2ba98b49a1c38dcaac356ed9673ee715
James Dumay <james@atlassian.com>
2014-05-27 10:58:20 +1000

    something

--- src/main/java/com/watercooler/feedle/Entry.java
+++ src/main/java/com/watercooler/feedle/Entry.java
@@ -16,12 +16,7 @@ public class Entry
     public String getTitle()
     {
         return title;
-    }
-
-    public void setTitle(String title)
-    {
-        this.title = title;
-    }
+		
 
     public String getLink()
     {
